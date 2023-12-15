6c1a4dbf197784f59bbbc1974dc224953507e90e
Garry <personarpg7501@gmail.com>
2023-12-15 23:55:36 +0900

    [0.0.1] server : 서버 시작 listen 작성, port(8080) 변경

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // main 폴더의 main.html 서빙
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main', 'main.html'));
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
