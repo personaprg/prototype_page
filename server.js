4a0ad5500becef1dc0183824c431a8b7303398c7
Garry <personarpg7501@gmail.com>
2023-12-15 23:54:14 +0900

    [0.0.1] server : main 폴더 main.html 서빙 테스트

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+
+// main 폴더의 main.html 서빙
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main', 'main.html'));
+});
