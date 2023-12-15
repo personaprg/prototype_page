51b3f880b01232124f22a26600efab10acc9972b
Garry <personarpg7501@gmail.com>
2023-12-15 23:59:10 +0900

    [0.0.1] server : 정적파일 서빙코드 작성 및 테스트 - 이슈작성 server 및 번들링 테스트

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-// main 폴더의 main.html 서빙
+// main 폴더의 main.html 서빙코드
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main', 'main.html'));
 });
+
+// data 폴더의 정적 파일 서빙코드
+app.use('/data', express.static(path.join(__dirname, 'data')));
 
 // 서버 시작
 app.listen(port, () => {
