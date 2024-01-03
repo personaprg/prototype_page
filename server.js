9e5a33394f3de0e2916cd0d0eb59085cfc2bb202
DongHyunShin <personarpg7501@gmail.com>
2024-01-03 19:26:38 +0900

    test : json 데이터에 따라서 동적으로 버튼이 생성 될 수 있는지 테스트

--- server.js
+++ server.js
@@ -5,11 +5,23 @@ const app = express();
 const port = 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-// Define routes
+// html 파일 서빙
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/main.html'));
+});
+
+// json 데이터 처리
+app.get('/getLocations', (req, res) => {
+    try {
+        const locationData = fs.readFileSync('public/location.json', 'utf8');
+        const locations = JSON.parse(locationData);
+        res.json(locations);
+    } catch (err) {
+        console.error('Error reading location.json file', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 // Start the server
